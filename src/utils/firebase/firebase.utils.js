c7fc3a7276315a15d51bb3db3cd98efe17d7f5e2
Sergio Vieira da Costa <sergiovieiradacosta@Sergios-MacBook-Pro.local>
2023-06-20 22:05:54 +0100

    Added the user.saga

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
+import { initializeApp } from 'firebase/app';
 import {
-    getAuth,
-    signInWithRedirect,
-    signInWithPopup,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
-
 import {
   getFirestore,
   doc,
@@ -20,14 +18,13 @@ import {
   collection,
   writeBatch,
   query,
-  getDocs
+  getDocs,
 } from 'firebase/firestore';
 
-// https://firebase.google.com/docs/web/setup#available-libraries
 
+// https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -38,92 +35,39 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-    prompt:"select_account"
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
-
-export const signInWithGoogleRedirect = async () => {
-  const {user} = await signInWithRedirect(auth,googleProvider);
-  console.log({user});
-
-};
-
-//firestore
+  //firestore
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth, additionalInformation) => {
- 
-  if(!userAuth) return;
-
-  const userDocRef = doc(db,'users', userAuth.uid); 
-
-  const userSnapshot = await getDoc(userDocRef);
-  
-  if(!userSnapshot.exists()){
-
-    try {
-    
-      const { displayName, email } = userAuth;
-      const createAt = new Date();
-
-      await setDoc(userDocRef,{
-        displayName,
-        email,
-        createAt,
-        ...additionalInformation
-      });
-
-    } catch (error) {
-      console.error('error create the user', error.message);
-    }
-
-  }
-  return userDocRef;
-
-};
-
-export const createAuthUserWithEmailAndPassword = async (email, password)=>{
-
-  if(!email || !password) return;
-
-  return await createUserWithEmailAndPassword(auth,email,password);
-};
-
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
-
-  return await signInWithEmailAndPassword(auth, email, password);
-};
-
-export const signOutUser = async () => await signOut(auth);
-
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
 
 /*
 Observable
 onAuthStateChanged(auth, callback, errorCallback, completeCallbacx);
 */
-
 export const addCollectionAndDocuments = async (
   collectionKey,
-  objectsToAdd
+  objectsToAdd,
+  field
 ) => {
-  //Open transaction
-  const batch = writeBatch(db);
   const collectionRef = collection(db, collectionKey);
-  
+   //Open transaction
+  const batch = writeBatch(db);
   objectsToAdd.forEach((object) => {
-     const docRef = doc(collectionRef, object.title.toLowerCase());
-     batch.set(docRef, object);
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
   });
 
   // Commit Transaction
@@ -136,13 +80,58 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  return querySnapshot.docs.map((doc) => doc.data());
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 };
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userSnapshot;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
 
 
 ///Libries javascript changes all the time, it is a best practise to use or create
 //a method to encapsulate it, so when they changes, you have only one place to change.
-
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
     const unsubscribe = onAuthStateChanged(
