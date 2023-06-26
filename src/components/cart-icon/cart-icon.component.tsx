45380b0dec8092698075326975b6ebe01fe7941f
Sergio Vieira da Costa <sergiovieiradacosta@Sergios-MacBook-Pro.local>
2023-06-26 20:08:20 +0100

    migrated the component from jsx to typescript

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -5,14 +5,16 @@ import {
   selectIsCartOpen,
 } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
   const dispatch = useDispatch();
-  const isCartOpen = useSelector(selectIsCartOpen);
+
   const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
