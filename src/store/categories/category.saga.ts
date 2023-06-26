768c89eed01a8224773fb8e9dd7e0d670a223e13
Sergio Vieira da Costa <sergiovieiradacosta@Sergios-MacBook-Pro.local>
2023-06-26 19:16:11 +0100

    created user store saga

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga/macro';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
@@ -12,26 +12,25 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
 //We don't have async and await when we're working with generetors
 export function* fetchCategoriesAsync() {
   try {
-
     //call: is used to call a function
-    const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
 
     //Sagas are able to fire actions, but isted of use dispatch, we use put. put === dispatch
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    yield* put(fetchCategoriesSuccess(categoriesArray));
 
   } catch (error) {
-    //if some error happens, please distpach "put", an action.
-    yield put(fetchCategoriesFailed(error)); 
+     //if some error happens, please distpach "put", an action.
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
-  yield takeLatest(
+  yield* takeLatest(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
     fetchCategoriesAsync
   );
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,8 +1,9 @@
 import { createSelector } from 'reselect';
-import { RootState } from '../store';
-import { UserState } from './user.reducer';
 
-export const selectUserReducer = (state:RootState): UserState => state.user;
+import { UserState } from './user.reducer';
+import { RootState } from '../store';
+
+export const selectUserReducer = (state: RootState): UserState => state.user;
 
 export const selectCurrentUser = createSelector(
   selectUserReducer,
